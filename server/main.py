f905a03fbe852a4415d82fb383805f2943dd09bd
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2018-05-10 18:30:09 +0200

    added gracefull stopping.

--- server/main.py
+++ server/main.py
@@ -8,6 +8,7 @@ import server_pb2_grpc
 
 from session import Session
 
+import signal
 import log
 import os
 
@@ -15,16 +16,26 @@ import os
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+
 def serve():
+    killer = GracefulKiller()
+
     logger = log.setup_custom_logger('cos301-DND')
 
     logger.info('Starting...')
 
     if os.environ['ENV'] == 'prod':
         logger.info('In production enviroment!')
-
-    #logger.info("Connecting to sqlite databse...")
-    #db.connect()
+    else:
+        logger.info('In development enviroment!')
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     server_pb2_grpc.add_SessionsManagerServicer_to_server(Session(), server)
@@ -35,10 +46,17 @@ def serve():
 
     try:
         while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
+            if killer.kill_now:
+                logger.info('Received kill, stopping...')
+                break
+            time.sleep(1)
+
     except KeyboardInterrupt:
         server.stop(0)
-
+        logger.info('Stopped!')
+    finally:
+        server.stop(0)
+        logger.info('Stopped!')
 
 if __name__ == '__main__':      
     serve()
