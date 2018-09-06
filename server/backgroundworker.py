1f800a73081e12fb61d893e07d5fe817886d303c
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2018-09-06 12:32:56 +0200

    Added error handling.

--- server/backgroundworker.py
+++ server/backgroundworker.py
@@ -20,16 +20,23 @@ class BackgroundWorker:
 
     # Routine that processes whatever you want as background
     def BackgroundCleaner(self):
-        while True:
-            time.sleep(5)
-            self.conn = self._connectDatabase()
-            users = self.conn.query(db.User).filter(and_(
-                db.User.date_updated < datetime.datetime.now() - datetime.timedelta(
-                    seconds=5), db.User.online == True))
-            for _user in users:
-                    _user.online = False
-                    self.logger.info("User: " + _user.name + " is now offline!")
-                    self.conn.commit()
+        try:
+            while True:
+                time.sleep(5)
+                self.conn = self._connectDatabase()
+                users = self.conn.query(db.User).filter(and_(
+                    db.User.date_updated < datetime.datetime.now() - datetime.timedelta(
+                        seconds=5), db.User.online == True))
+                for _user in users:
+                        _user.online = False
+                        self.logger.info("User: " + _user.name + " is now offline!")
+                        self.conn.commit()
+        except exc.SQLAlchemyError as err:
+            self.logger.error("SQLAlchemyError! " + str(err))
+        except Exception as err:
+            self.logger.exception("Unhandled exception occurred! " + str(err))
+        finally:
+            self.conn.close()
 
     def start(self):
         t1 = threading.Thread(target=self.BackgroundCleaner)
